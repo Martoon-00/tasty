c2f95c73bb525ed5a382432fd9c76423bedd72e8
martoon <martoon.hsk@gmail.com>
2018-11-23 17:16:12 +0300

    Do not handle sigfault signals

--- core/Test/Tasty/CmdLine.hs
+++ core/Test/Tasty/CmdLine.hs
@@ -87,7 +87,7 @@ installSignalHandlers = do
 #if INSTALL_HANDLERS
   main_thread_id <- myThreadId
   weak_tid <- mkWeakThreadId main_thread_id
-  forM_ [ sigABRT, sigBUS, sigFPE, sigHUP, sigILL, sigQUIT, sigSEGV,
+  forM_ [ sigABRT, sigBUS, sigFPE, sigHUP, sigILL, sigQUIT,
           sigSYS, sigTERM, sigUSR1, sigUSR2, sigXCPU, sigXFSZ ] $ \sig ->
     installHandler sig (Catch $ send_exception weak_tid sig) Nothing
   where
